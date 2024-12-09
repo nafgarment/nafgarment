6ec2d6ce2d799390fcbd322dd8da6346ef9068e4
tabishkhan03 <tabish25u@gmail.com>
2024-12-09 19:09:53 +0530

    added vercel.json

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use((error, req, res, next) => {
 });
 
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 });
 
